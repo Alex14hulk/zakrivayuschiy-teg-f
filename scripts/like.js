4df8402b12ede773406b086f7ecd20e98c2b284f
Alexander Leontev <mr.leo.alexandr@yandex.ru>
2024-03-02 16:18:52 +0500

    final_1

--- scripts/like.js
+++ scripts/like.js
@@ -9,8 +9,8 @@
 */
 
 const likeHeartArray = document.querySelectorAll('.like-icon');
-const likeButtonArray = document.querySelectorAll('.card__like-button');
-const iconButtonArray = document.querySelectorAll('.card__icon-button');
+const likeButtonArray = document.querySelectorAll('.card__like');
+const iconButtonArray = document.querySelectorAll('.card__icon');
 
 iconButtonArray.forEach((iconButton, index) => {
   iconButton.onclick = () =>
